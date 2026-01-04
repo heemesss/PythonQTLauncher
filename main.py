2edfa42603be075bfe29462b79f0b73b2d968e02
heemes <semenluts90@gmail.com>
2026-01-05 00:22:49 +0600

    first

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 from PyQt6 import QtWidgets, uic
+from UI import Ui_MainWindow
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('example.ui', self)
+        super().setupUi(self)
+        # uic.loadUi('example.ui', self)
 
 
 if __name__ == "__main__":
